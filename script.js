4d8a9b847d50220f285af0abce93680bc2a01cd4
Stephanie Lin <stephlinya@gmail.com>
2021-12-06 15:32:25 -0800

    added mobile slideshow

--- script.js
+++ script.js
@@ -11,37 +11,75 @@ mobileNav.addEventListener("click", function (){
 
 let dot = document.getElementsByClassName("fa-circle");
 let slides = document.getElementsByClassName("slides");
+let imgs = document.getElementsByClassName("gallery-img");
 let slideIndex = 0;
 let dotIndex = 0;
 
-
-function slideshow(){
-    for (i = 0; i < slides.length; i++){
-        slides[slideIndex].style.display = "none";
-        dot[dotIndex].classList.add("pink");
-    
-        if (slideIndex != 0){
-            dot[dotIndex-1].classList.remove("pink");
-        } else if (slideIndex === 0){
-            dot[dotIndex].classList.add("pink");
-            slides[slideIndex].style.display = "block";
-        }
-        else if (slideIndex === 4){
+if (slideIndex <= slides.length){
+    setInterval(function(){ 
+        
+        if (slideIndex === 0){
+            imgs[slides.length-1].style.display = "none";
+            imgs[slideIndex].style.display = "block";
+        } else if (slideIndex != 0 && slideIndex < 4){
+            imgs[slideIndex].style.display = "block";
+            imgs[slideIndex - 1].style.display = "none";
+        } else if (slideIndex === slides.length-1){
             slideIndex = 0;
-            dotIndex = 0;
-            dot[dotIndex].classList.add("pink");
-            slides[slideIndex].style.display = "block";
-        } else {
-            dot[dotIndex].classList.add("pink");
-            slides[slideIndex].style.display = "block";
-        }
+            console.log(slides.length-1);
+            imgs[slideIndex].style.display = "block";
+        } 
+    
         slideIndex++;
-        dotIndex++;
+        
 
-        console.log(slideIndex);
-        console.log(dotIndex);
-    }
+    }, 2000);
+    
 }
+
+// if (dotIndex == 0){
+        //     dot[dotIndex].classList.add("pink");
+        // } else if (dotIndex === slides.length){
+        //     dot[dotIndex].classList.remove("pink");
+        //     dotIndex = 0;
+        // } else {
+        //     dot[dotIndex].classList.add("pink");
+        //     dot[dotIndex - 1].classList.remove("pink");
+        // }
+        // dotIndex++;
+        // console.log(dotIndex);
+        // console.log(slides.length);
+
+
+// function slideshow(){
+//     for (i = 0; i < slides.length; i++){
+//         slides[slideIndex].style.display = "none";
+//         dot[dotIndex].classList.add("pink");
+    
+//         if (slideIndex != 0){
+//             dot[dotIndex-1].classList.remove("pink");
+//             dot[dotIndex].classList.add("pink");
+//             slides[slideIndex-1].style.display = "none";
+//             slides[slideIndex].style.display = "block";
+//         } else if (slideIndex === 0){
+//             dot[dotIndex].classList.add("pink");
+//             slides[slideIndex].style.display = "block";
+//         }
+//         else if (slideIndex === 4){
+//             slideIndex = 0;
+//             dotIndex = 0;
+//             dot[dotIndex].classList.add("pink");
+//             slides[slideIndex].style.display = "block";
+//         } 
+
+//         slideIndex++;
+//         dotIndex++;
+
+//     }
+// }
+
+
+
 
 
 
