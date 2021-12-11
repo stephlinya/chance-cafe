61894de745ca55142a6ad8c497519cb2bf0ef880
Stephanie Lin <stephlinya@gmail.com>
2021-12-10 16:02:07 -0800

    changed font styles to free for personal use, added sticky nav and transition for mobile nav list

--- script.js
+++ script.js
@@ -6,8 +6,21 @@ mobileNav.addEventListener("click", function (){
         navList.style.display ="none";
     } else {
         navList.style.display="flex";
+        navList.classList.add("nav__list--animate")
     }
 })
+
+window.onscroll = function() {
+    if (window.pageYOffset >= sticky) {
+        mobileNav.classList.add("sticky")
+    } else {
+        mobileNav.classList.remove("sticky");
+    }
+} 
+
+const sticky = mobileNav.offsetTop;
+
+
 
 let dot = document.getElementsByClassName("fa-circle");
 let slides = document.getElementsByClassName("slides");
@@ -32,16 +45,16 @@ setInterval(function(){
     } 
     
     if (dotIndex === 0){
-        dot[slides.length-1].classList.remove("pink");
-        dot[dotIndex].classList.add("pink");
+        dot[slides.length-1].classList.remove("green");
+        dot[dotIndex].classList.add("green");
         dotIndex++;
     } else if (dotIndex === slides.length-1){
-        dot[dotIndex].classList.add("pink");
-        dot[dotIndex - 1].classList.remove("pink");
+        dot[dotIndex].classList.add("green");
+        dot[dotIndex - 1].classList.remove("green");
         dotIndex = 0;
     } else {
-        dot[dotIndex].classList.add("pink");
-        dot[dotIndex - 1].classList.remove("pink");
+        dot[dotIndex].classList.add("green");
+        dot[dotIndex - 1].classList.remove("green");
         dotIndex++;
     }
 
