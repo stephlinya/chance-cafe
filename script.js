19d9b7ddd5f242b20b2e9411e814cde698d7658d
Stephanie Lin <stephlinya@gmail.com>
2022-01-06 14:33:16 -0800

    added more information on adoption process and created modal for adoption requirements

--- script.js
+++ script.js
@@ -62,15 +62,39 @@ $(function(){
             imgs[slideIndex].style.display = "none";
         })
     }
-  });
-
-const adoptionChecklist = document.querySelector(".adoption__checklist");
-const checklistOverlay = document.querySelector(".overlay__checklist");
-
-adoptionChecklist.addEventListener("click", (e) => {
-    e.preventDefault();
-    // adoptionChecklist.classList.add("overlay__checklist");
-    // checklistOverlay.innerHTML = `<p style="z-index:3; color: white">heyyyyy</p>`
 });
+
+const adoptionRequirements = document.querySelector(".adoption__requirements");
+const checklistOverlay = document.querySelector(".overlay__checklist");
+const closeRequirements = document.getElementById("close-requirements");
+
+adoptionRequirements.addEventListener("click", (e) => {
+    e.preventDefault();
+    adoptionRequirements.classList.add("overlay__checklist");
+    adoptionRequirements.classList.remove("link");
+    adoptionRequirements.innerHTML = `
+    <button id="close-requirements">x</button>
+    <h1>Adoption Requirements<h2>
+    <ul id="requirement-list">
+        <li>1. Fill out an adoption interest form.</li>
+        <li>2. Complete an adoption intereview.</li>
+        <li>3. If you're approved, set an adoption day.</li>
+        <li>4. Get your house prepared for your new buddy.</li>
+        <li>5. Bring your kitty home on adoption day!</li>
+    </ul>
+
+    <h2>Adoption process</h2>
+    `
+
+    closeRequirements.addEventListener("click", (e) => {
+        adoptionRequirements.classList.remove("overlay__checklist");
+        adoptionRequirements.classList.add("link");
+        console.log("closed");
+    })
+});
+
+
+
+
 
         
